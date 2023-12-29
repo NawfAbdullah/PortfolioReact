b7ce917509dede4764a1c6bfc911218c63a7d111
Nawf Abdullah <k.nawfabdullah@gmail.com>
2023-12-29 19:32:49 +0530

    self driving car added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,6 +2,7 @@ import '../styles/Project.css'
 import Pilot from '../assets/images/projects/pilot.png'
 import Club from '../assets/images/projects/cisc.png'
 import TerminalIcon from '@mui/icons-material/Terminal';
+import Self from '../assets/images/projects/self.png'
 import {motion} from 'framer-motion';
 
 
@@ -43,7 +44,7 @@ const Projects = () => {
                         <div className="box" style={{background:"#00CA4E"}}></div>
                     </div>
                     <div className="screen">
-                        <img src={Club} alt="" />
+                        <img src={Self} alt="" />
                     </div>
                     <h4><TerminalIcon/>Self Driving Car</h4>
                 </div>
