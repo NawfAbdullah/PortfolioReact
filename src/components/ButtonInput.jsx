22d1c2d6acc798a63588ffd6926596302aa76bb3
Nawf Abdullah <k.nawfabdullah@gmail.com>
2023-12-29 19:28:58 +0530

    projects removed

--- src/components/ButtonInput.jsx
+++ src/components/ButtonInput.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import '../styles/ButtonInput.css'
 
 const ButtonInput = ({placeholder,onClick,btn_text}) => {
     const [data,setData] = useState("")
-    
+    const ref = useRef()
     return (  
         <div className="btn-input">
             <input type="text" onChange={(e)=>setData(e.target.value)} value={data} placeholder={placeholder} />
-            <button onClick={()=>onClick(data)}>{btn_text}</button>
+            <button onClick={()=>{onClick(data);ref.current.click()}}>{btn_text}</button>
+            <a ref={ref} href="mailto:k.nawfabdullah@gmail.com"></a>
         </div>
     );
 }
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,6 @@ import '../styles/Project.css'
 import Pilot from '../assets/images/projects/pilot.png'
 import Club from '../assets/images/projects/cisc.png'
 import TerminalIcon from '@mui/icons-material/Terminal';
-import CircularButton from './Buttons/CircularButton';
 import {motion} from 'framer-motion';
 
 
@@ -36,22 +35,19 @@ const Projects = () => {
                     <h4><TerminalIcon/>CISC Club official website</h4>
                 </div>
             </motion.div>
-            {/* <div className="project odd">
-                <motion.img initial={{x:window.innerWidth>600?-400:-100,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:1.2}} src={Pilot} alt="" />
-                <motion.div className="pro-details" initial={{x:window.innerWidth>600?400:100,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:1.2}}>
-                    <h4>Pilot Admin Portal</h4> 
-                    <p className="tagline">React.js</p>
-                    <CircularButton style={{borderRadius:20,height:60}}>View</CircularButton>
-                </motion.div>
-            </div>
-            <div className="project even">
-                <motion.img initial={{x:window.innerWidth>600?400:100,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:1.2}} src={Club} alt="" />
-                <motion.div className="pro-details" initial={{x:window.innerWidth>600?-400:-100,opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:1.2}}>
-                    <h4>CISC Club official website</h4>
-                    <p className="tagline">MERN Stack</p>
-                    <CircularButton style={{borderRadius:20,height:60}}>View</CircularButton>
-                </motion.div>
-            </div> */}
+            <motion.div className="project">
+                <div className="outline">
+                    <div className="controls">
+                        <div className="box" style={{background:"#FF605C"}}></div>
+                        <div className="box" style={{background:"#FFBD44"}}></div>
+                        <div className="box" style={{background:"#00CA4E"}}></div>
+                    </div>
+                    <div className="screen">
+                        <img src={Club} alt="" />
+                    </div>
+                    <h4><TerminalIcon/>Self Driving Car</h4>
+                </div>
+            </motion.div>
         </div>
      );
 }
